1ea9d5588283469fb2014e970dca788a48221da0
Juan Jose Pineda Sanchez <jpinedasa@unal.edu.co>
2024-06-26 02:07:37 -0500

    frontend done 10

--- frontend_react/src/api/ventas.api.js
+++ frontend_react/src/api/ventas.api.js
@@ -1,7 +1,7 @@
 // ventas.api.js
 import axios from 'axios';
 
-const URL = process.env.NODE_ENV === "production" ? import.meta.env.VITE_BACKEND_URL : "http://127.0.0.1:8000/";
+const URL = process.env.NODE_ENV === "production" ? import.meta.env.VITE_BACKEND_URL : "http://127.0.0.1:8000";
 
 const ventasApi = axios.create({
   baseURL: `${URL}/ventas/api/`
